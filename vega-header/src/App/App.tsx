be863a4f2eaa32f101ed741ea75856082ed8cf44
Sergey Gavshin <inzephirum@gmail.com>
2020-09-17 16:47:40 +0700

    доработать header

--- vega-header/src/App/App.tsx
+++ vega-header/src/App/App.tsx
@@ -1,79 +1,79 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, useHistory, useLocation } from 'react-router-dom';
 import { Header, NavItemType } from '@gpn-prototypes/vega-header';
 import { Text } from '@gpn-prototypes/vega-text';
 import { Root as VegaRoot } from '@gpn-prototypes/vega-root';
 
-export function App(props) {
-  props.fetch();
+interface NavLinkType extends NavItemType {
+  url: string;
+}
 
-  const navItems = [
+function VegaHeader() {
+  const history = useHistory();
+  const location = useLocation();
+
+  const navItems: NavLinkType[] = [
     {
       name: 'Экономика проекта',
       url: '/fem',
-      isActive: true,
+      isActive: location.pathname.startsWith('/fem'),
     },
     {
       name: 'Логика проекта',
       url: '/logic',
+      isActive: location.pathname.startsWith('/logic')
     },
   ];
+
   const menuItems = [
     { name: 'Проекты', url: '/' },
-    { name: 'Обучение', url: '' },
-    { name: 'Помощь', url: 'help' },
+    { name: 'Обучение', url: '/' },
+    { name: 'Помощь', url: '/help' },
   ];
 
-  const [activeItem, setActiveItem] = React.useState(navItems.find((ni) => ni.isActive));
+  const activeItem = navItems.find((ni) => ni.isActive);
 
-  // const handleChangeActive = (item: NavItemType): void => {
-  //   setActiveItem(item);
-  // };
+  const handleChangeActive = (item: NavLinkType): void => {
+    history.push(item.url);
+  };
 
   return (
-    <BrowserRouter basename="projects/">
-      <VegaRoot defaultTheme="dark">
-        <Header>
-          <Header.Menu title="Вега">
-            {menuItems.map((menuItem) => (
-              <Header.Menu.Item key={menuItem.name}>
-                {(menuItemProps): React.ReactNode => (
-                  <Link
-                    onClick={menuItemProps.closeMenu}
-                    className={menuItemProps.className}
-                    to={menuItem.url}
-                  >
-                    <Text>{menuItem.name}</Text>
-                  </Link>
-                )}
-              </Header.Menu.Item>
-            ))}
-            <Header.Menu.Delimiter />
-            <Header.Menu.Item>
-              {(menuItemProps): React.ReactNode => (
-                <Link
-                  onClick={menuItemProps.closeMenu}
-                  className={menuItemProps.className}
-                  to="/login/"
-                >
-                  <Text>Выйти</Text>
-                </Link>
-              )}
-            </Header.Menu.Item>
-          </Header.Menu>
-          <div style={{ display: 'flex', width: 400 }}>
-            {navItems.map((item) => (
-              <Link to={item.url} style={{ color: '#fff', marginRight: 10 }}>
-                {item.name}
+    <Header>
+      <Header.Menu title="Вега">
+        {menuItems.map((menuItem) => (
+          <Header.Menu.Item key={menuItem.name}>
+            {(menuItemProps): React.ReactNode => (
+              <Link
+                onClick={menuItemProps.closeMenu}
+                className={menuItemProps.className}
+                to={menuItem.url}
+              >
+                <Text>{menuItem.name}</Text>
               </Link>
-            ))}
-          </div>
-          {/* <Header.Nav
-            navItems={navItems}
-            activeItem={activeItem}
-            onChangeItem={handleChangeActive}
-          /> */}
-        </Header>
+            )}
+          </Header.Menu.Item>
+        ))}
+        <Header.Menu.Delimiter />
+        <Header.Menu.Item>
+          {(menuItemProps): React.ReactNode => (
+            <Link onClick={menuItemProps.closeMenu} className={menuItemProps.className} to="/login">
+              <Text>Выйти</Text>
+            </Link>
+          )}
+        </Header.Menu.Item>
+      </Header.Menu>
+      <Header.Nav activeItem={activeItem} navItems={navItems} onChangeItem={handleChangeActive} />
+    </Header>
+  );
+}
+
+export function App(props) {
+  props.fetch();
+
+  return (
+    <BrowserRouter basename="/projects">
+      <VegaRoot defaultTheme="dark">
+        <VegaHeader />
       </VegaRoot>
     </BrowserRouter>
   );
